ed2e37f36287fb4d882d83733ea93acb8d02dd76
Aleksey Galatin <aleksey_galatin@epam.com>
2020-06-28 16:58:16 +0300

    Created JWT Authentication Manager

---
+++ ConferenceSystem/Application/Auth/IAuthenticationManager.cs
@@ -0,0 +1,14 @@
+﻿using ConferenceSystem.Application.JwtSettings;
+using ConferenceSystem.Models;
+using System.Threading.Tasks;
+
+namespace ConferenceSystem.Application.Auth
+{
+    public interface IAuthenticationManager
+    {
+        Task<JwtToken> Authenticate(LoginDto loginDto);
+        Task<User> GetCurrentUser();
+        Task<JwtToken> RegisterAsync(CreateUserDto createUserDto);
+        Task<JwtToken> RefreshToken(JwtToken jwtToken);
+    }
+}
